93fae39c95e830968113ced6dac158dca2dce797
Abu-0-RayhaN <rayhan844625@gmail.com>
2023-11-01 22:48:20 +0600

    tailwind and daisy ui set up

---
+++ src/components/About.jsx
@@ -0,0 +1,7 @@
+// import React from 'react'
+
+const About = () => {
+  return <div>About page goes here 🌐</div>;
+};
+
+export default About;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,28 @@
+// import React from 'react'
+
+const Nav = () => {
+  return (
+    <>
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <a href="">home</a>
+            </li>
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Nav;
